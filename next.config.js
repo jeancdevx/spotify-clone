c6ed0619448876da21bc2aa922b1a53a111f477e
jcodev2 <jcode2006@gmail.com>
2023-06-12 18:27:20 -0500

    add domains in next config

--- next.config.js
+++ next.config.js
@@ -1,4 +1,12 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
+const nextConfig = {
+  images: {
+    domains: ['wakhinayldoqohzrhapl.supabase.co']
+  }
+}
 
 module.exports = nextConfig
+
+// https://wakhinayldoqohzrhapl.supabase.co/storage/v1/object/public/songs/image-All%20of%20me-lis07xir
+
+// https://wakhinayldoqohzrhapl.supabase.co/storage/v1/object/public/images/image-Some%20kind%20of%20feelin-lis0hs4e.png
