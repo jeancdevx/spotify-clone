4d4a1770a4c6c750802e8fcb44ead76f8dd54bee
jcodev2 <jcode2006@gmail.com>
2023-06-12 18:28:47 -0500

    add domains on nextjs config

--- next.config.js
+++ next.config.js
@@ -6,7 +6,3 @@ const nextConfig = {
 }
 
 module.exports = nextConfig
-
-// https://wakhinayldoqohzrhapl.supabase.co/storage/v1/object/public/songs/image-All%20of%20me-lis07xir
-
-// https://wakhinayldoqohzrhapl.supabase.co/storage/v1/object/public/images/image-Some%20kind%20of%20feelin-lis0hs4e.png
