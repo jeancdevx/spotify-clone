8d1797a5e28713ca49007f0d743c714c0998e5a2
jcodev2 <jcode2006@gmail.com>
2023-06-16 09:07:21 -0500

    add get active products server component function

---
+++ actions/getActiveProductsWithPrices.js
@@ -0,0 +1,25 @@
+import { createServerComponentClient } from '@supabase/auth-helpers-nextjs'
+import { cookies } from 'next/headers'
+
+const getActiveProductsWithPrices = async () => {
+  const supabase = createServerComponentClient({
+    cookies
+  })
+
+  const { data, error } = await supabase
+    .from('products')
+    .select('*, prices(*)')
+    .eq('active', true)
+    .eq('prices.active', true)
+    .order('metadata->index')
+    .order('unit_amount', { foreignTable: 'prices' })
+
+  if (error) {
+    console.error(error)
+    return []
+  }
+
+  return data
+}
+
+export default getActiveProductsWithPrices
