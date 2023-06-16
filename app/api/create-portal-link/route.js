7ed8eadf15356e79dc409f075f62d6e8c52871d9
jcodev2 <jcode2006@gmail.com>
2023-06-16 09:09:41 -0500

    add api web hooks for stripe payment

---
+++ app/api/create-portal-link/route.js
@@ -0,0 +1,37 @@
+import { getURL } from '@/libs/helpers'
+import { stripe } from '@/libs/stripe'
+import { createOrRetrieveCustomer } from '@/libs/supabaseAdmin'
+import { createRouteHandlerClient } from '@supabase/auth-helpers-nextjs'
+import { cookies } from 'next/headers'
+import { NextResponse } from 'next/server'
+
+export async function POST() {
+  try {
+    const supabase = createRouteHandlerClient({
+      cookies
+    })
+
+    const {
+      data: { user }
+    } = await supabase.auth.getUser()
+
+    if (!user) throw Error('Could not get user')
+
+    const customer = await createOrRetrieveCustomer({
+      uuid: user.id || '',
+      email: user.email || ''
+    })
+
+    if (!customer) throw Error('Could not get customer')
+    const { url } = await stripe.billingPortal.sessions.create({
+      customer,
+      return_url: `${getURL()}/account`
+    })
+
+    return NextResponse.json({ url })
+  } catch (err) {
+    console.log(err)
+
+    return new NextResponse('Internal Error', { status: 500 })
+  }
+}
